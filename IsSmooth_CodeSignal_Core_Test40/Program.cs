5302ad01fa433af5d742eb81cc726f39100ee1eb
Albert Hakobyan <abhakobyan24@gmail.com>
2021-09-11 18:19:58 +0400

    Добавьте файлы проекта.

---
+++ IsSmooth_CodeSignal_Core_Test40/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsSmooth_CodeSignal_Core_Test40
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { -12, 34, 40, -5, -12, 4, 0, 0, -12 };
+            bool action = isSmooth(array);
+            Console.WriteLine(action);
+            
+        }
+
+        static bool isSmooth(int[] arr)
+        {
+            int midle;
+            int midleleft;
+            int midleright;
+            if (arr.Length % 2 == 1)
+            {
+                midle = arr.Length / 2;
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[0] == arr[arr.Length - 1] && arr[0] == arr[midle])
+                    {
+                        return true;
+                    }
+                }
+            }
+            else
+            {
+                midleright = arr.Length / 2;
+                midleleft = midleright - 1;
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[0] == arr[arr.Length - 1] && arr[0] == arr[midleright] + arr[midleleft])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+    }
+}
